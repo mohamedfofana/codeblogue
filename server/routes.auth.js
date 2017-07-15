af3ad58eb49fb555901e363fd4c71a98e208456c
Mohamed Fofana <manoufof@hotmail.com>
2017-07-15 19:12:22 +0200

    CSS refactor and activate contact href

--- server/routes.auth.js
+++
@@ -1,7 +0,0 @@
-// tableau
-// el1: url
-// el2: controller à appeler
-module.exports = {
-	'/api/auth/google' : './server/controllers/google',	
-	'/api/auth/google/callback' : './server/controllers/googleC'
-};
