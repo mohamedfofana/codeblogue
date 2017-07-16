e2ca02fbe13fe7f724af1fd88d2693ced917c126
Mohamed Fofana <manoufof@hotmail.com>
2017-07-16 17:35:43 +0200

    CSS refactor

--- server/controllers/google.js
+++
@@ -1,9 +0,0 @@
-module.exports = function(passport) {
-    console.log("google");
-    return passport.authenticate('google', { scope : ['profile', 'email'] });
-    // On retourn le middleware
-	/*return function(req, res, next){
-		next();
-	};
-    */
-}
--- server/controllers/googleC.js
+++
@@ -1,9 +0,0 @@
-module.exports = function(passport) {
-      console.log("googleC");
-    passport.authenticate('google', { successRedirect: '/profile', failureRedirect: '/' });
-    // On retourn le middleware
-	/*return function(req, res, next){
-		next();
-	};
-    */
-}
